2ff8117354289222e8ec7501b7be50e7d6adf425
jagalmon <85403653+jagalmon@users.noreply.github.com>
2024-12-10 10:07:46 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 EXE_BACKEND = "Local" # IBM: IBM Quantum 하드웨어
-H2_ANGSTROM = 0.74
-BOND_MIN = 0.5
-BOND_MAX = 2.5
-BOND_COUNT = 20
-NUM_ELECTRONS = 2
-NUM_SPATIAL_ORBITALS = 2
+H2_ANGSTROM = 0.74 # 산소 분자의 평형 결합 거리(equilibrium bond length): 에너지가 최소화되는 가장 안정한 결합 길이 (단위: Å)
+BOND_MIN = 0.5 # 결합 길이의 최소값 (단위: Å)
+BOND_MAX = 2.5 # 결합 길이의 최대값 (단위: Å)
+BOND_COUNT = 20 # 결합 길이의 분할 갯수
+NUM_ELECTRONS = 2 # 분자의 전자 개수
+NUM_SPATIAL_ORBITALS = 2 # 공간적 궤도 수
